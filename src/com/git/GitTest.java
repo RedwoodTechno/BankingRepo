1e5f19eda740338070bf89e304d2226597353b09
Dineshkumar <Consultantdineshkumar@gmail.com>
2024-01-30 18:02:52 +0530

    adjkh

--- src/com/git/GitTest.java
+++ src/com/git/GitTest.java
@@ -3,8 +3,7 @@ package com.git;
 public class GitTest {
 
 	public static void test() {
-		System.out.println(5 + 5);
-		System.out.println(10+10);
+
 		System.out.println("Hello");
 		System.out.println("Git");
 		System.out.println("Welcome");
