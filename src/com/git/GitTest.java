8599e667451206ec1e472239ba80978795a5651f
Dineshkumar <Consultantdineshkumar@gmail.com>
2024-01-30 17:12:31 +0530

    sffffffd

--- src/com/git/GitTest.java
+++ src/com/git/GitTest.java
@@ -4,8 +4,8 @@ public class GitTest {
 
 	public static void test() {
 		System.out.println(5 + 5);
-		System.out.println("Hello");
-		System.out.println("Git");
+		System.out.println("HHHHHH");
+		System.out.println("GGGG");
 	}
 
 	public static void main(String[] args){
